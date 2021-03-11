ff7db5e7749a86e8e43b26f29d2ca1665aac18bf
Domenico <ferraro.domenico125@gmail.com>
2021-03-11 12:51:56 +0100

    simple example with one producer and one consumer

--- examples/simpleprodcons.c
+++ examples/simpleprodcons.c
@@ -16,26 +16,24 @@
 #define PRODUCER_FILEPATH "./tmp/prod/mypipe.txt"
 #define CONSUMER_FILEPATH "./tmp/cons/mypipe.txt"
 #define MAXNUMBERS 10
-#define READ_END 0
-#define WRITE_END 1
 
 /**
  * Function executed by the producer process
  */
-static int producer(int fd) {
-    int prec1 = 0, prec2 = 1, next = 1, counter = 2, maxnumbers = MAXNUMBERS;
+static int producer() {
+    int fd, prec1 = 0, prec2 = 1, next = 1, counter = 2, maxnumbers = MAXNUMBERS;
 
     // Open file
-    MINUS1ERR(fd = open(PRODUCER_FILEPATH, O_WRONLY), return 1);
+    MINUS1ERR(fd = open(PRODUCER_FILEPATH, O_WRONLY), return EXIT_FAILURE)
 
     // Send the first MAXNUMBERS
-    ISNEGATIVEERR(writen(fd, &maxnumbers, sizeof(int)), return 1)
+    ISNEGATIVEERR(writen(fd, &maxnumbers, sizeof(int)), return EXIT_FAILURE)
     printf("Writing %d %d ", prec1, prec2);
-    ISNEGATIVEERR(writen(fd, &prec1, sizeof(int)), return 1)
-    ISNEGATIVEERR(writen(fd, &prec2, sizeof(int)), return 1)
+    ISNEGATIVEERR(writen(fd, &prec1, sizeof(int)), return EXIT_FAILURE)
+    ISNEGATIVEERR(writen(fd, &prec2, sizeof(int)), return EXIT_FAILURE)
 
     while (counter < MAXNUMBERS) {
-        ISNEGATIVEERR(writen(fd, &next, sizeof(int)), return 1)
+        ISNEGATIVEERR(writen(fd, &next, sizeof(int)), return EXIT_FAILURE)
         printf("%d ", next);
 
         prec1 = prec2;
@@ -44,31 +42,33 @@ static int producer(int fd) {
         counter++;
     }
     next = -1;
-    ISNEGATIVEERR(writen(fd, &next, sizeof(int)), return 1)
+    ISNEGATIVEERR(writen(fd, &next, sizeof(int)), return EXIT_FAILURE)
     printf("-1 into %s\n", PRODUCER_FILEPATH);
 
     // Close
-    return close(fd);
+    MINUS1ERR(close(fd), return EXIT_FAILURE);
+    return 0;
 }
 
 /**
  * Function executed by the consumer process
  */
-static int consumer(int fd) {
-    int read, maxnumbers;
+static int consumer() {
+    int fd, read, maxnumbers;
 
     // Open file
-    MINUS1ERR(fd = open(CONSUMER_FILEPATH, O_RDONLY), return 1);
+    MINUS1ERR(fd = open(CONSUMER_FILEPATH, O_RDONLY), return EXIT_FAILURE);
 
     // Read from it
-    ISNEGATIVEERR(read = readn(fd, &maxnumbers, sizeof(int)), return 1)
+    ISNEGATIVEERR(read = readn(fd, &maxnumbers, sizeof(int)), return EXIT_FAILURE)
     if (read == 0) {
         printf("End of file\n");
         return 0;
     }
     int *numbers = (int*) malloc(sizeof(int)*maxnumbers);
-    EQNULL(numbers, perror("malloc"); return 1);
-    ISNEGATIVEERR(read = readn(fd, numbers, sizeof(int)*maxnumbers), return 1)
+    EQNULL(numbers, perror("malloc"); return EXIT_FAILURE)
+
+    ISNEGATIVEERR(read = readn(fd, numbers, sizeof(int)*maxnumbers), return EXIT_FAILURE)
     if (read == 0) {
         printf("End of file\n");
         return 0;
@@ -80,28 +80,21 @@ static int consumer(int fd) {
     printf("from %s\n", CONSUMER_FILEPATH);
 
     // Close
-    return close(fd);
+    MINUS1ERR(close(fd), return EXIT_FAILURE)
+    return 0;
 }
 
 int main(int argc, char** argv) {
     int pid_cons;
-    int pipefd[2] = { 0,0 };
-
-    // Create an unnamed pipe
-    //MINUS1ERR(pipe(pipefd), return 1)
 
     // Fork a process to run the consumer
-    MINUS1ERR(pid_cons = fork(), return 1)
+    MINUS1ERR(pid_cons = fork(), return EXIT_FAILURE)
 
-    if (pid_cons > 0) {
-        return consumer(pipefd[READ_END]);
+    if (pid_cons == 0) {
+        return consumer();
     }
 
-    NOTZERO(producer(pipefd[WRITE_END]), perror("consumer"))
-
-    // Close the unnamed pipe
-    //MINUS1(close(pipefd[READ_END]), perror("close pipe's read end"))
-    //MINUS1(close(pipefd[WRITE_END]), perror("close pipe's write end"))
-
-    return waitpid(pid_cons, NULL, 0);
+    int ret = producer();
+    MINUS1(waitpid(pid_cons, NULL, 0), fprintf(stderr, "failure to wait pid %d: ", pid_cons); perror(""); return EXIT_FAILURE)
+    return ret;
 }
