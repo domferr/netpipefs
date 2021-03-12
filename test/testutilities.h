a0668fb9a95b0dae1848eeb0b91125e907931b86
Domenico <ferraro.domenico125@gmail.com>
2021-03-12 11:58:00 +0100

    more utilities and error printing

--- test/testutilities.h
+++ test/testutilities.h
@@ -2,13 +2,20 @@
 #define TESTUTILITIES_H
 
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define PASS "[ PASS ]"
+#define FAIL "[ FAIL ]"
+#define SPCE "        "
 
 #define test(expr)       \
-    if (!(expr)) {      \
-        fprintf(stderr, "[ FAIL ] %s:%d: test '%s' failed\n", __FILE__, __LINE__, #expr);   \
+    if (!(expr)) {       \
+        fprintf(stderr, "%s %s:%d: test '%s' failed\n", FAIL, __FILE__, __LINE__, #expr); \
+        if (errno != 0) fprintf(stderr, "%s %s\n", SPCE, strerror(errno));                \
         exit(EXIT_FAILURE); \
     }
 
-#define testpassed(test) fprintf(stdout, "[ PASS ] %s\n", test)
+#define testpassed(test) fprintf(stdout, "%s %s\n", PASS, test)
 
 #endif //TESTUTILITIES_H
